8c39f47711eee37559f72367b2d3762874cfbd06
gmf <gmf0010@uah.edu>
2026-02-17 05:12:38 -0600

    save compiled temp files

--- operation.s
+++ operation.s
@@ -1,3 +1,4 @@
+@ this ARM assembly file was written by hand
 .global sum_squares
 .global square_root_approx
 
@@ -7,10 +8,13 @@ sum_squares:
 	ADD r0, r0, r1
 	BX LR
 
-square_root_approx:
+square_root_approx: @ terrible approximation, Newton's method
 	MOV r1, r0, LSR #1
-	PUSH {r1, LR}
+	PUSH {r1, LR} @ save the parameter in case div overwrites
 	BL __aeabi_uidiv
+	@ ready for a mouthful? deep breaths
+	@ ARM Embedded Application Binary Interface,
+	@ Unsigned Integer Divide
 	POP {r1, LR}
 	ADD r0, r1, r0
 	LSR r0, #1
--- program.c
+++ program.c
@@ -7,6 +7,7 @@ int main() {
 	int i, j;
 	scanf("%d", &i);
 	scanf("%d", &j);
+	// must scanf twice to get two inputs
 	int x = sum_squares(i, j);
 	int y = square_root_approx(x);
 	return 0;
